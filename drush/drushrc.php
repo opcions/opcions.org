b58024b25ac0bcd0b8c8258cb4172d688c9bbf5e
Eduardo Esclapes <eduardo.esclapes@gmail.com>
2016-01-15 12:51:18 +0100

    drushrc was preventing devel extension status to be exported but devel settings were not hence a confict on import

--- drush/drushrc.php
+++ drush/drushrc.php
@@ -28,6 +28,6 @@
  * This facility allows you to manually decide whether these
  * modules should be enabled or disabled on a per-environment basis.
  */
-$command_specific['config-export']['skip-modules'] = array('devel');
-$command_specific['config-import']['skip-modules'] = array('devel');
+//$command_specific['config-export']['skip-modules'] = array('devel');
+//$command_specific['config-import']['skip-modules'] = array('devel');
 
